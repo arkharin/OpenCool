9d1d22fe9ae411c0fe5823f9a640cce0e6b91884
arkharin <arkharin@openaliasbox.org>
2017-04-22 11:36:47 +0200

    Clean Component class
    Eliminated old construction methods.
    Eliminated comented lines in __init__ Component class used in the old construction algorithm.

--- scr/logic/components/component.py
+++ scr/logic/components/component.py
@@ -49,30 +49,12 @@ class Component(ABC, Element):
 
         super().__init__(name, id_)
 
-        #check_type(data[self.COMPONENT_TYPE], str)
         self._component_library = component_type
         self._component_type = component_type.rsplit('.')[0]
-
-        #id_inlet_nodes = data[self.INLET_NODES]
-        #self._check_input_nodes(inlet_nodes_id, n_inlet_nodes)
-        #self._inlet_nodes = {x: circuit_nodes[x] for x in id_inlet_nodes}
         self._inlet_nodes = inlet_nodes_id
-
-        #id_outlet_nodes = data[self.OUTLET_NODES]
-        #self._check_input_nodes(id_outlet_nodes, n_outlet_nodes)
-        #self._outlet_nodes = {x: circuit_nodes[x] for x in id_outlet_nodes}
         self._outlet_nodes = outlet_nodes_id
-
-        #self._check_input_properties(data[self.BASIC_PROPERTIES], basic_properties_allowed)
-        #self._basic_properties = data[self.BASIC_PROPERTIES]
         self._basic_properties = {x: component_data[x] for x in basic_properties_allowed if x in component_data}
-
-        #self._check_input_properties(data[self.OPTIONAL_PROPERTIES], optional_properties_allowed)
-        #self._optional_properties = data[self.OPTIONAL_PROPERTIES]
         self._optional_properties = {x: component_data[x] for x in optional_properties_allowed if x in component_data}
-
-        #self._attach_to_nodes()
-
         self._basic_properties_results = self.NO_INIT
         self._optional_properties_results = self.NO_INIT
 
@@ -85,7 +67,6 @@ class Component(ABC, Element):
         self._outlet_nodes = {}
         for node_id in nodes_id:
             self._outlet_nodes[node_id] = nodes_dict[node_id]
-
 
 
     @staticmethod
@@ -103,37 +84,6 @@ class Component(ABC, Element):
         class_name = class_name.replace(class_name[0], class_name[0].upper(), 1)
         class_ = getattr(cmp, class_name)
         return class_(name, id_, component_type, inlet_nodes_id, outlet_nodes_id, component_data)
-
-    def _attach_to_nodes(self):
-        nodes = self.get_nodes()
-        for node in nodes:
-            nodes[node].attach_component(self)
-
-    def _check_input_properties(self, input_properties, keys_allowed):
-        # Keys allow is a dictionary with keys names = name properties and each key with a dictionary with lower_limit
-        # and upper_limit keys and values.
-        try:
-            check_type(input_properties, dict)
-        except TypeValueError:
-            print('Input properties are not a dictionary!')
-            exit(1)
-
-        for key in input_properties:
-            if key in keys_allowed:
-                    check_type(input_properties[key][self.VALUE], float)
-                    check_input_float(input_properties[key][self.VALUE], keys_allowed[key][self.LOWER_LIMIT],
-                                      keys_allowed[key][self.UPPER_LIMIT])
-                    check_input_str(input_properties[key][self.UNIT], keys_allowed[key][self.UNIT])
-            else:
-                raise PropertyNameError(
-                    "Invalid property. %s  is not in %s]" % keys_allowed)
-
-    def _check_input_nodes(self, nodes, n_nodes):
-        check_type(nodes, list)
-        if len(nodes) == n_nodes:
-            return
-        else:
-            raise ValueError('Number of nodes is wrong. %s is not %s' % (len(nodes), n_nodes))
 
     @abstractmethod
     def _calculated_result(self, key):
