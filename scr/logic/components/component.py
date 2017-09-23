59c4a9596c7dddd1aeea99f1bdf1872c786ebf54
Unknown <arkharin@openaliasbox.org>
2017-09-23 19:27:49 +0200

    Clean up Component
    Clean up of Component file. 
    Some big TODO left to be solved later.

--- scr/logic/components/component.py
+++ scr/logic/components/component.py
@@ -6,25 +6,21 @@
 Define the abstract class component.
 """
 import inspect
-from abc import ABC, abstractmethod
-from importlib import import_module
-from collections import namedtuple
-
+from abc import ABC
 from scr.helpers.properties import StrRestricted
 from scr.logic.base_classes import Element
-from scr.logic.errors import TypeValueError, PropertyNameError, BuildError, ComponentBuilderError, DeserializerError
+from scr.logic.errors import ComponentBuilderError, DeserializerError
 from scr.logic.warnings import ComponentBuilderWarning, ComponentWarning
 from importlib import import_module
 from scr.helpers.singleton import Singleton
 
-
-# TODO new form to evaluated the error need to be implemented.
-
 ''' Decorators to use in component plugins to register them in ComponentFactory and ComponentInfoFactory '''
+
+
 # See question 5929107 (python decorators with parameters) in stackoverflow
-# See question 2366713 (Can a Python decorator of an instance method access the class?) in StackOverflow to know about how to
-# solve the problem that method decorators are called when class doesn't exist yet. I implemented the same solution
-# used by flask-classy. Be aware that methods in classes are bounded functions and therefore points to closure
+# See question 2366713 (Can a Python decorator of an instance method access the class?) in StackOverflow to know about
+# how to solve the problem that method decorators are called when class doesn't exist yet. I implemented the same
+# solution used by flask-classy. Be aware that methods in classes are bounded functions and therefore points to closure
 # variables and are share between instances
 
 
@@ -34,17 +30,18 @@ def component(key, component_type, version=1, updater_data_func=None):
         if not issubclass(cls, Component):
             raise ValueError('This decorator can be use only to decorate components subclasses')
 
-        if version != 1 and updater_data_func == None:
+        if version != 1 and updater_data_func is None:
             raise ValueError('updater_func must be distinct to None if version is not one')
 
-        cmp_info = ComponentInfo(key, cls, component_type, component_version=version, updater_data_func=updater_data_func)
+        cmp_info = ComponentInfo(key, cls, component_type, component_version=version,
+                                 updater_data_func=updater_data_func)
 
         # Search those functions in the class that has been decorated with *_property decorator and add info in
         # to component info
         for member in vars(cls).values():
             if callable(member) and hasattr(member, '_property_type'):
                 if member._property_type == 'fundamental':
-                    pass # It's an equation, not a property. It doesn't have name.
+                    pass  # It's an equation, not a property. It doesn't have name.
                 elif member._property_type == 'basic':
                     cmp_info.add_basic_property(member._property_name, member._property_value)
                 elif member._property_type == 'auxiliary':
@@ -56,6 +53,7 @@ def component(key, component_type, version=1, updater_data_func=None):
         ComponentFactory().add(key, cls)
 
         return cls
+
     return real_decorator
 
 
@@ -69,38 +67,41 @@ def fundamental_equation():
 
 def basic_property(**kwargs):
     def real_decorator(func):
-        if len(kwargs) != 1 :
-            raise ValueError('basic_property decorator must be called with one keyword argument that it will be the property name')
+        if len(kwargs) != 1:
+            raise ValueError('basic_property decorator must be called with one keyword argument that it will be the '
+                             'property name')
         # Because we are lazy. It's the fastest way to have the key and value. Only iterate one time
         for property_name, value in kwargs.items():
             setattr(func, '_property_name', property_name)
-            setattr(func, '_property_type', 'basic' )
+            setattr(func, '_property_type', 'basic')
             setattr(func, '_property_value', value)
 
         return func
+
     return real_decorator
 
 
 def auxiliary_property(**kwargs):
     def real_decorator(func):
-        if len(kwargs) != 1 :
-            raise ValueError('auxiliary_property decorator must be called with one keyword argument that it will be the property name')
+        if len(kwargs) != 1:
+            raise ValueError('auxiliary_property decorator must be called with one keyword argument that it will be the'
+                             ' property name')
         # Because we are lazy. It's the fastest way to have the key and value. Only iterate one time
         for property_name, value in kwargs.items():
             setattr(func, '_property_name', property_name)
-            setattr(func, '_property_type', 'auxiliary' )
+            setattr(func, '_property_type', 'auxiliary')
             setattr(func, '_property_value', value)
 
         return func
+
     return real_decorator
+
 
 ''' End of the decorators to use in component plugins to register them in ComponentFactory and ComponentInfoFactory '''
 
 
 class Component(ABC, Element):
     # Arbritary value to check used to check if super method is called in _register_xxx_property_eq() methods
-    # FIXME search better name
-    NO_INIT = None
     # TODO pasar a component info
     # Main types
     COMPRESSOR = 'compressor'
@@ -133,7 +134,7 @@ class Component(ABC, Element):
         for s_attribute in dir(self):
             attribute = getattr(self, s_attribute)
             if callable(attribute) and hasattr(attribute, '_property_type'):
-                if hasattr(attribute,'_property_name'):
+                if hasattr(attribute, '_property_name'):
                     property_name = attribute._property_name
                     if not hasattr(self, property_name):
                         setattr(self, property_name, None)
@@ -174,23 +175,6 @@ class Component(ABC, Element):
         for node_id in nodes_id:
             self._outlet_nodes[node_id] = nodes_dict[node_id]
 
-
-    @staticmethod
-    def build(name, id_, component_type, inlet_nodes_id, outlet_nodes_id, component_data):
-        # Dynamic importing modules
-        cmp_type = component_type
-        try:
-            cmp = import_module('scr.logic.components.' + cmp_type)
-        except ImportError:
-            print('Error loading component. Type: %s is not found', cmp_type)
-            exit(1)
-        aux = component_type.rsplit('.')
-        class_name = aux.pop()
-        # Only capitalize the first letter
-        class_name = class_name.replace(class_name[0], class_name[0].upper(), 1)
-        class_ = getattr(cmp, class_name)
-        return class_(name, id_, component_type, inlet_nodes_id, outlet_nodes_id, component_data)
-
     def eval_equations(self):
         # Return a matrix of two columns with the calculation result of each side of the equation.
         results = []
@@ -212,14 +196,10 @@ class Component(ABC, Element):
         elif key in self.get_auxiliary_properties():
             self._auxiliary_properties_results[key] = self._auxiliary_eqs[key]()
         else:
-            raise ComponentWarning('%s property is not allowed in %s component' % (key, self.get_component_info().get_component_type()))
-
+            raise ComponentWarning('%s property is not allowed in %s component' % (key, self.get_component_info().
+                                                                                   get_component_type()))
 
     # General methods:
-    def get_id_input_properties(self):
-        # Return a dictview with the names of inputs of properties
-        return self._basic_properties.keys()
-
     def get_basic_properties(self):
         # Return an array of dictionaries. Each dictionary in the format of example output components to interface.
         return self._basic_properties
@@ -239,10 +219,6 @@ class Component(ABC, Element):
     def get_property(self, key):
         return getattr(self, key)
 
-    def get_type(self):
-        # TODO where will be use?
-        return
-
     def get_inlet_nodes(self):
         """
         Return a dictionary of all inlet nodes of the component, keys ordered by lowest to higher pressure.
@@ -251,17 +227,17 @@ class Component(ABC, Element):
         return self._inlet_nodes
 
     def get_id_inlet_nodes(self):
-            return list(self.get_inlet_nodes().keys())
+        return list(self.get_inlet_nodes().keys())
 
     def get_inlet_node(self, id_node):
-            return self.get_inlet_nodes()[id_node]
+        return self.get_inlet_nodes()[id_node]
 
     def get_nodes(self):
         # Return a dictionary with all nodes connected with the component. First inlet nodes.
         return {**self.get_inlet_nodes(), **self.get_outlet_nodes()}
 
     def get_node(self, id_node):
-            return self.get_nodes()[id_node]
+        return self.get_nodes()[id_node]
 
     def get_outlet_nodes(self):
         # Return nodes in the same order criterion of get_inlet_nodes
@@ -317,10 +293,10 @@ class AComponentSerializer(ABC):
         for node_id in cmp_data[self.OUTLET_NODES]:
             cmp.add_outlet_node(i, node_id)
             i += 1
-        for basic_property in cmp_data[self.BASIC_PROPERTIES]:
-            cmp.set_attribute(basic_property, cmp_data[self.BASIC_PROPERTIES][basic_property])
-        for auxiliary_property in cmp_data[self.AUXILIARY_PROPERTIES]:
-            cmp.set_attribute(auxiliary_property, cmp_data[self.AUXILIARY_PROPERTIES][auxiliary_property])
+        for key in cmp_data[self.BASIC_PROPERTIES]:
+            cmp.set_attribute(key, cmp_data[self.BASIC_PROPERTIES][key])
+        for key in cmp_data[self.AUXILIARY_PROPERTIES]:
+            cmp.set_attribute(key, cmp_data[self.AUXILIARY_PROPERTIES][key])
 
         return cmp
 
@@ -346,6 +322,7 @@ class AComponentSerializer(ABC):
 
 
 class ComponentBuilder:
+    # TODO check if builder check all input data.
     def __init__(self, id_, component_type):
         self._name = None
         self._id = id_
@@ -374,8 +351,8 @@ class ComponentBuilder:
         if None in self._outlet_nodes_id:
             raise ComponentBuilderError('Missing nodes attached to the outlet of the component %s.', self.get_id())
 
-        # TODO check the correctness of the data throw component info retrieved with ComponentFactory
-        return ComponentFactory().create(self.get_component_type(), self._name, self._id, self._inlet_nodes_id, self._outlet_nodes_id, self._component_data)
+        return ComponentFactory().create(self.get_component_type(), self._name, self._id, self._inlet_nodes_id,
+                                         self._outlet_nodes_id, self._component_data)
 
     def set_name(self, name):
         self._name = StrRestricted(name)
@@ -434,10 +411,10 @@ class ComponentBuilder:
         return self._inlet_nodes_id + self._outlet_nodes_id
 
     def has_node(self, node_id):
-        #TODO check if is used
         return (node_id in self._inlet_nodes_id) or (node_id in self._outlet_nodes_id)
 
     def set_attribute(self, attribute_name, value):
+        # TODO check if exist and the value is numeric
         self._component_data[attribute_name] = value
 
     def remove_attribute(self, attribute_name):
@@ -446,6 +423,7 @@ class ComponentBuilder:
 
     def get_component_type(self):
         return self._component_type.get()
+
 
 class ComponentFactory(metaclass=Singleton):
     def __init__(self):
@@ -482,8 +460,7 @@ class ComponentFactory(metaclass=Singleton):
 class ComponentInfo:
     def __init__(self, component_key, component_class, component_type, component_version=1, updater_data_func=None):
         self._component_key = component_key
-        # FIXME make attribute private
-        self.component_class = component_class
+        self._component_class = component_class
         self._component_type = component_type
         self._parent_component_class = inspect.getmro(component_class)[1]  # Parent class
         self._component_version = component_version
@@ -494,13 +471,9 @@ class ComponentInfo:
         self._auxiliary_properties_info = {}
 
     def _add_property(self, dict_to_save, property_name, property_value):
-
-        # TODO check if two properties never have the same name (Altough one is fundamental and the other auxliar)
-        if property_name in dict_to_save:
+        if property_name in self._basic_properties_info or property_name in self._auxiliary_properties_info:
             raise ValueError('Property ' + str(property_name) + ' has already been registered in ' + str(type(self)))
 
-            # PropertyInfo = namedtuple('PropertyInfo','value func_name')
-            # dict_to_save[property_name] = PropertyInfo(property_value, property_func_name)
         dict_to_save[property_name] = property_value
 
     def add_basic_property(self, property_name, property_value):
@@ -526,10 +499,10 @@ class ComponentInfo:
 
     def get_parent_component_class(self):
         # Return the parent class. DO NOT WORK with multiple inheritance
-        return inspect.getmro(self.component_class)[1]
+        return inspect.getmro(self._component_class)[1]
 
     def get_component_class(self):
-        return self.component_class
+        return self._component_class
 
     def get_component_key(self):
         return self._component_key
@@ -555,7 +528,8 @@ class ComponentInfoFactory(metaclass=Singleton):
 
             keyed_cls = self.get(key).get_component_class()
 
-            if not (mod_spec.origin == inspect.getmodule(keyed_cls).__spec__.origin and component_class.__name__ == keyed_cls.__name__):
+            if not (mod_spec.origin == inspect.getmodule(
+                    keyed_cls).__spec__.origin and component_class.__name__ == keyed_cls.__name__):
                 raise ValueError('key' + str(key) + ' has already been registered in ' + str(type(self)))
         else:
             if component_class in self._components_info:
@@ -563,11 +537,10 @@ class ComponentInfoFactory(metaclass=Singleton):
             self._components_info[component_class] = component_info
             self._components_info[key] = component_info
 
-
     def get(self, key):
         """
-        Return the ComponentInfo registered for a specific key. 
-            key must be a string, type or a instance of Component class. In this case, the class name will be used as key.  
+        Return the ComponentInfo registered for a specific key.
+        Key must be a string, type or a instance of Component class. In this case, the class name will be used as key.
         """
         if isinstance(key, Component):
             key = key.__class__
